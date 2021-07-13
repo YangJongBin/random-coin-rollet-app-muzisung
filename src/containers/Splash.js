7724e5bfb55d4f06165f793887480bb7e1ebd2ec
YangJongBin <toutopia0@gmail.com>
2021-07-14 01:05:13 +0900

    splash 화면 작업

--- src/containers/Splash.js
+++ src/containers/Splash.js
@@ -1,7 +1,8 @@
 import React, {useCallback, useEffect} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, View, Image} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {useNavigation} from '@react-navigation/native';
+import LottieView from 'lottie-react-native';
 
 import {getBithumbCoinList} from '../modules/bithumb';
 import _ from 'lodash';
@@ -22,7 +23,7 @@ export default function Splash() {
   useEffect(() => {
     setTimeout(() => {
       reqBithumbCoinList();
-    }, 1000);
+    }, 2500);
   }, []);
 
   // if(_.toNumber(resInfo.status) < 500){
@@ -30,10 +31,29 @@ export default function Splash() {
   // }
 
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.content}>
+      <LottieView
+        source={require('../lottie/loading2.json')}
+        loop
+        style={styles.loading}
+        autoPlay></LottieView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#74b9ff',
+  },
+
+  loading: {
+    width: '10%',
+    marginLeft: 7,
+    marginBottom: 7,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
