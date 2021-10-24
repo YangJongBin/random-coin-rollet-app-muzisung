0bad1c91f267e2a73232356ec3e2695894a365de
YangJongBin <toutopia0@gmail.com>
2021-10-24 21:13:40 +0900

    release/2.0.1

--- src/containers/Splash.js
+++ src/containers/Splash.js
@@ -1,10 +1,9 @@
 import React, {useCallback, useEffect, version} from 'react';
-import {Text, StyleSheet, View, Image} from 'react-native';
+import {Text, StyleSheet, View, Image, Alert} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {useNavigation} from '@react-navigation/native';
 import LottieView from 'lottie-react-native';
 import _ from 'lodash';
-import VersionCheck from 'react-native-version-check';
 
 import {getUpbitMarketAllList} from '../modules/upbit/upbitMarketAll';
 
